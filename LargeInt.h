a036734f1054a7efc996f94f68c1c8f62211fb4e
kayak28 <kaya_ota@msn.com>
2013-04-20 01:35:45 -0700

    add inherited files

---
+++ LargeInt.h
@@ -0,0 +1,15 @@
+#include"IntList.h"
+class LargeInt: public IntList
+{
+	public:
+		LargeInt();
+		int getLargeVal();
+
+		LargeInt(const LargeInt&);
+		LargeInt& operator=(const LargeInt &);
+		void insert(int val);
+
+	private:
+		int largeVal;
+
+}
